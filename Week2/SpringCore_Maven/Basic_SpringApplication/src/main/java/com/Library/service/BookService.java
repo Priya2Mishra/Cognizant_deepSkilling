09cd3e7b6115effaed0af0cf22250b1ad661420e
Priya Mishra <shrijimishra6@gmail.com>
2026-07-13 10:25:28 +0530

    please add my basic spring application project

---
+++ Week2/SpringCore_Maven/Basic_SpringApplication/src/main/java/com/Library/service/BookService.java
@@ -0,0 +1,18 @@
+package com.Library.service;
+
+import com.Library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository repository;
+
+    // Setter Injection
+    public void setRepository(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    public void displayService() {
+        System.out.println("Book Service is working.");
+        repository.displayRepository();
+    }
+}
